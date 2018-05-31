8f662d980b2630698d0cf6a8ccef7a3836b52475
benjibennn <benjaminwong94@hotmail.my>
2018-05-31 12:21:27 +0800

    added quote api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :lists
   get 'sessions/new'
-  resources :homes
+  resources :homes, except: :show
   resources :users, except: :show
   get "/users/login", to: 'users#login'
   get "/users/logout", to: 'users#logout'
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   get '/logout',  to: 'sessions#destroy'
   get "/auth/google_oauth2/callback" => "sessions#create_from_omniauth"
+  get '/redirect', to: 'sessions#redirect', as: 'redirect'
+  get '/callback', to: 'sessions#callback', as: 'callback'
+    get 'homes/quote', to: 'homes#quote', as: 'quote'
 
   root to: "homes#index"
 end
